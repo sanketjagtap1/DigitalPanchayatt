d9bb432e2b06014e6226e68a4075d19bc7c76929
Sanket jagtap <sanketjagtap479@gmail.com>
2023-06-10 18:49:35 +0530

    1. filter and pagination

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IonicModule } from '@ionic/angular';
+import { ManageStaffComponent } from './manage-staff/manage-staff.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, ManageStaffComponent, ManageUserComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    IonicModule.forRoot()
+    IonicModule.forRoot(),
+    NgxPaginationModule,
+    FormsModule
   ]
 })
 export class AdminModule { }
--- src/app/admin/manage-staff/manage-staff.component.ts
+++ src/app/admin/manage-staff/manage-staff.component.ts
@@ -1,14 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-manage-staff',
   templateUrl: './manage-staff.component.html',
   styleUrls: ['./manage-staff.component.scss'],
 })
-export class ManageStaffComponent  implements OnInit {
+export class ManageStaffComponent implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
+  @ViewChild('addStaffForm') addStaffForm!: NgForm;
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
 
-  constructor() { }
+  email: any;
+  password: any;
+  firstName: any;
+  lastName: any;
+  mobile: any;
+  UserRole = 'Staff';
+  users: any
+  p:number=1;
+  constructor() {
 
-  ngOnInit() {}
+  }
+
+  ngOnInit() {
+    this.users = [
+      {
+        sr: 1,
+        fname: "Sanket",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "8806328987",
+      },
+      {
+        sr: 2,
+        fname: "rahul",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "9022898699",
+      },
+      {
+        sr: 3,
+        fname: "akash",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "774383",
+      },
+      {
+        sr: 4,
+        fname: "mohit",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "915628",
+      },
+      {
+        sr: 5,
+        fname: "rahul",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "7272",
+      },
+      {
+        sr: 5,
+        fname: "rahul",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "7373",
+      },
+      {
+        sr: 6,
+        fname: "rahul",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "7474",
+      },
+      {
+        sr: 7,
+        fname: "rahul",
+        lname: "Sanket",
+        email: "Sanket",
+        mobile: "7575",
+      },
+    ]
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm(data: any) {
+    this.modal.dismiss('confirm');
+    console.log(data)
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+
+
+    }
+  }
+
+
+  Search() {
+    if (this.firstName == '') {
+      this.ngOnInit();
+    } else {
+      this.users = this.users.filter((res: { mobile: string; }) => {
+        return RegExp(this.firstName.toLocaleLowerCase()).exec(res.mobile.toLocaleLowerCase())
+      })
+    }
+  }
 
 }
